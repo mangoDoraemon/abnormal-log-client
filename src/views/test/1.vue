c46c9654b82ea4b5e31f729fcaf55b55593259b0
wangjingyuan <690397098@qq.com>
2020-11-19 17:17:28 +0800

    代码模型

--- src/views/test/1.vue
+++ src/views/test/1.vue
@@ -1,13 +1,65 @@
+
 <template>
-    <h1>22323232323</h1>
+  <div class="app-container">
+    <el-row>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>学件基础信息</span>
+        </div>
+        <div style="margin-bottom: 50px">
+          <el-form :model="formData" label-width="80px" size="medium">
+            <el-row gutter="250">
+              <el-col :span="10">
+                <el-form-item label="活动名称">
+                  <el-input
+                    style="
+                       {
+                        width: '50%';
+                      }
+                    "
+                  ></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="10">
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+
+              <el-col :span="10">
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+
+                            <el-col :span="10">
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+
+                            <el-col :span="10">
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </div>
+      </el-card>
+    </el-row>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "1"
-    }
+export default {
+  data() {
+    return {
+      input: "",
+      formData: {},
+    };
+  },
+};
 </script>
-
 <style scoped>
-
 </style>
